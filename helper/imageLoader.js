c9b05238b3d20b3ad5fc17281000164ed4420358
Amanda Jericho <ajericho@gmail.com>
2021-11-29 17:04:47 -0500

    added dynamic domain

--- helper/imageLoader.js
+++ helper/imageLoader.js
@@ -1,6 +1,6 @@
-const domain = window.location.host;
 
 
 export const imageLoader = ({ src, width, quantity }) => {
+  const domain = window.location.host;
     return `https://${domain}/${src}?w=${width}&q=${quantity || 75}`
   }
